8a0b005b466609d2b46635dbb04ab1f1bf4f1cc0
Ashish Vishwakarma <ashishvishwakarma7519@gmail.com>
2024-03-04 14:32:08 +0530

    Initial commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Stack } from "expo-router";
+
+const Root = () => {
+  return (
+    <Stack />
+  )
+}
+
+export default Root
---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Password Manager</Text>
+    </View>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
